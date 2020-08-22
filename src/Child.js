28c21a13cfbb2ad14b21928771fd21bcb8e0d9c6
inseob jeon <pravda@pravdalaptop-2.local>
2020-08-22 20:14:13 +0900

    return null

--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-// return event handler to child component
-function Child(props) {
-    return ( 
-    <>
-        <div>i got name {props.name}</div>
-        <button onClick={props.handler}>Button</button>
-    </>
-    
-    )
-};
+function LoginButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    );
+}
+  
+  function LogoutButton(props) {
+    return (
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    );
+}
 
+function UserGreeting(props) {
+    return <h1>Welcome back!</h1>;
+}
+  
+  function GuestGreeting(props) {
+    return <h1>Please sign up.</h1>;
+}
 
-export default Child
+function Greeting(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <UserGreeting />;
+    }
+    return <GuestGreeting />;
+}
+
+// 이렇게 {} 를 통해서 여러개의 function 을 내보낼 수도 있다. 
+export {LoginButton, LogoutButton, Greeting, UserGreeting, GuestGreeting};
