b70b61285e06932797c8495452e3dd2362cc76ae
inseob jeon <pravda@pravdalaptop-2.local>
2020-08-22 20:29:52 +0900

    rendering with map(1)

--- src/index.js
+++ src/index.js
@@ -2,44 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import {LoginButton, LogoutButton, Greeting} from './Child';
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
+function List() {
+  const numbers = [1, 2, 3, 4]
   return (
-    <div className="warning">
-      Warning!
-    </div>
-  );
-}
-
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {showWarning: true};
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
-
-  handleToggleClick() {
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn={this.state.showWarning} />
-        <button onClick={this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
+    numbers.map(arg => {
+      return <div>{arg}</div>
+    })
+  )
 }
 
 ReactDOM.render(
-  <Page />,
+  <List />,
   document.getElementById('root')
 );
